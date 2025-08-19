52375c7b374b8ad8c3af1e68bd24670bfe16efba
agentauraapp <agentauraapp@gmail.com>
2025-08-19 11:03:31 -0400

    Add files via upload
    new repo

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Agent Aura MVP</h1>
+    <p>Welcome to the MVP dashboard</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
