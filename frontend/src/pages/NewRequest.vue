c4af19fe67f93c27af3e9d80194f5db69b69863e
agentauraapp <agentauraapp@gmail.com>
2025-08-22 14:15:39 -0400

    Update NewRequest.vue

--- frontend/src/pages/NewRequest.vue
+++ frontend/src/pages/NewRequest.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>Hello from New Request</h1>
+  <section style="padding:24px; background:#fff">
+    <h1 style="color:#111; font-size:28px">Hello from New Request</h1>
+    <p>This should be visible at <code>/requests/new</code>.</p>
+  </section>
 </template>
