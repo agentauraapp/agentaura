c9784f86ca0d0e6f092c211a65519d02eaa06720
agentauraapp <agentauraapp@gmail.com>
2025-08-22 14:19:31 -0400

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+
 import App from './App.vue'
 import router from './router'
 
+// PrimeVue v3 styles (make sure you’re on primevue@^3)
 import 'primeflex/primeflex.css'
 import 'primeicons/primeicons.css'
-import 'primevue/resources/themes/lara-light-indigo/theme.css' // pick any v3 theme
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
 import 'primevue/resources/primevue.min.css'
 
-createApp(App).use(createPinia()).use(router).use(PrimeVue).mount('#app')
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(PrimeVue)
+  .mount('#app')
