5f6fe60099ed250596acddd5504256c65f542c91
agentauraapp <agentauraapp@gmail.com>
2025-08-22 12:42:10 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
+// frontend/vite.config.ts
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'node:path'
 
 export default defineConfig({
   plugins: [vue()],
-resolve: {
-  alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
